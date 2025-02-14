650cc00f45291b03ee7a1cf709331d4044bdd56c
Parth Raheja <parthraheja1205@gmail.com>
2025-02-15 00:43:46 +0530

    Update AnalyticsContent

--- WBPA Frontend/src/components/apiutils.ts
+++ WBPA Frontend/src/components/apiutils.ts
@@ -1,4 +1,4 @@
-const YOUR_API_KEY = "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";
+const YOUR_API_KEY = "your_api_key";
 
 export const fetchHealthPredictions = async (waterQualityData) => {
     const prompt = `Based on the following water quality parameters, predict potential health risks in 3-4 concise bullet points:\n\n${JSON.stringify(waterQualityData, null, 2)}`;
