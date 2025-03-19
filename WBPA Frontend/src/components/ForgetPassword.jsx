d943e3d4f4e65fa30ecf1032ae83db00ad348210
Parth Raheja <parthraheja1205@gmail.com>
2025-03-19 14:52:12 +0530

    Update Files

--- WBPA Frontend/src/components/ForgetPassword.jsx
+++ WBPA Frontend/src/components/ForgetPassword.jsx
@@ -11,7 +11,7 @@ const ForgetPassword = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            await axios.post('http://localhost:5000/api/auth/forgot-password', { email });
+            await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/auth/forgot-password`, { email });
             toast.success("Password reset link sent to your email.",{position: "bottom-right"})
             // setTimeout(() => {
             //   navigate('/reset-password');
